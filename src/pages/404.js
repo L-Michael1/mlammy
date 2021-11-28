37151ca304abd705fbe31865486d80dfbe4d265b
Michael Lam <mlam11@uoguelph.ca>
2021-11-28 00:23:45 -0500

    updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,16 +3,31 @@ import Layout from "../components/layout/layout"
 import Seo from "../components/seo/seo"
 import Typewriter from "../components/typewriter/typewriter"
 import styled from "styled-components"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Description = styled.p`
   color: #FFFFFF;
+  text-align: center;
+`
+
+const Header = styled(Typewriter)`
+  text-align: center;
 `
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <Typewriter text="404: Not Found" textColor="#FFE8D6" initDelay={0} minTypeSpeed={60} maxTypeSpeed={70} />
+    <Header text="404: Not Found" textColor="#FFE8D6" initDelay={0} minTypeSpeed={60} maxTypeSpeed={70} />
     <Description>You just entered a nonexistent world...oh, so sad.</Description>
+    <StaticImage
+      src="../images/404.png"
+      width={200}
+      height={200}
+      quality={100}
+      formats={["auto", "webp", "avif"]}
+      alt="Sad anime character"
+      style={{ marginTop: `3rem`, borderRadius: `25px` }}
+    />
   </Layout>
 )
 
