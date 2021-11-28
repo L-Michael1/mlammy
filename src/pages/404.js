6cd7e1853cfc333411aa488d512c9899d3653b66
Michael Lam <mlam11@uoguelph.ca>
2021-11-28 12:34:03 -0500

    responsive 404 gif

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout/layout"
 import Seo from "../components/seo/seo"
 import Typewriter from "../components/typewriter/typewriter"
 import styled from "styled-components"
+import bepopGIF from '../gifs/404-smoke.gif'
 import { StaticImage } from "gatsby-plugin-image"
 
 const Description = styled.p`
@@ -10,19 +11,23 @@ const Description = styled.p`
   text-align: center;
 `
 
+const Gif = styled.img`
+  margin-top: 3rem;
+  margin-left: 2rem;
+  margin-right: 2rem;
+  border-radius: 25px;
+  width: 62%;
+  height: auto;
+`
+
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
     <Typewriter text="404: Not Found" textAlign="center" textColor="#FFE8D6" initDelay={0} minTypeSpeed={60} maxTypeSpeed={70} />
     <Description>You just entered a nonexistent world...oh, so sad.</Description>
-    <StaticImage
-      src="../images/404.png"
-      width={350}
-      height={350}
-      quality={100}
-      formats={["auto", "webp", "avif"]}
-      alt="Sad anime character"
-      style={{ marginTop: `3rem`, borderRadius: `25px` }}
+    <Gif
+      src={bepopGIF}
+      alt="Cowboy Bepop smoking gif"
     />
   </Layout>
 )
