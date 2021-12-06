5c7a245eb7dad3d712d27ee27edd5b6602e54eb3
Michael Lam <mlam11@uoguelph.ca>
2021-12-05 19:52:34 -0500

    grammar

--- src/pages/tulip-report.js
+++ src/pages/tulip-report.js
@@ -69,9 +69,9 @@ const TulipReport = () => {
                 into a digital setting. With Tulip’s solutions, retailers such as Chanel, Jimmy Choo, Kate
                 Spade, and many more retail stores have the ability to strengthen the relationship between
                 their brand and their customers. Tulip has multiple apps designed for iOS mobile devices for both sales
-                associates and retail customers such as Assisted Selling, Clientelling, and Live Connect.
+                associates and retail customers such as Assisted Selling, Clienteling, and Live Connect.
                 Culture at Tulip is fantastic, friendly and helpful co-workers, great management, and a fun
-                environment with a lot of non-work-related activities such as a weekly social gaming event, festivus,
+                environment with a lot of non-work-related activities such as a weekly social gaming event, Festivus,
                 and a yearly hackathon.
             </Content>
             <SubHeader>Goals</SubHeader>
@@ -83,39 +83,39 @@ const TulipReport = () => {
                     <li>Improve verbal communication and collaborative skills.</li>
                     <li>Be able to balance the hardships of work and live a normal life.</li>
                 </ul>
-                These were the goals I had upon entering Tulip, and I successfully completed two of them. With
-                regard to my first goal, I wanted to learn three new languages: PHP, Go, and Swift. It was a tough
+                These were the goals I had upon entering Tulip, and I completed two of them. Concerning
+                my first goal, I wanted to learn three new languages: PHP, Go, and Swift. It was a tough
                 goal for such a short work term. I developed my PHP skills quite nicely as it was the main language
                 I worked in, as for Go, I got some exposure to the language, and I found it super interesting! As for
-                Swift, I didn't get to touch because I never handled the iOS portion of things. This goal was definitely
-                an overreach, and I'll definitely learn to create goals that are more realistic and achievable in my
+                Swift, I didn't get to touch because I never handled the iOS portion of things. This goal was
+                an overreach, and I'll definitely learn to create goals that are more realistic and achievable in the
                 near future. Overall, these skills are all very much transferrable to my future, not only technically,
                 but also personally!
             </Content>
             <SubHeader>Job Description</SubHeader>
             <Content>
-                I was a part of the Checkout Team, working as Backend Developer solving a variety of problems and
+                I was a part of the Checkout Team, working as a Backend Developer solving a variety of problems and
                 bugs to increase customer satisfaction and to optimize the Checkout app. I worked mainly in PHP and MySQL, wrote
-                unit tests in Python, and did a few small things in Go. The main skills needed for this job was communication
-                and collaboration not only in my team, but also across other teams at Tulip. I found that these skills were not taught
+                unit tests in Python, and did a few small things in Go. The main skills needed for this job were communication
+                and collaboration not only in my team but also across other teams at Tulip. I found that these skills were not taught
                 specifically in my program, and I learned to better communicate and collaborate while on the job and through colleagues. What I found unique
                 about my job was that we worked super closely with Chanel on the Checkout team. A large part of our work items each sprint
-                were Chanel related, which I found very intriguing since I got to work with such a large brand!
+                were Chanel-related, which I found very intriguing since I got to work with such a large brand!
             </Content>
             <SubHeader>Conclusion</SubHeader>
             <Content>
                 It was a wonderful first Co-op work term, I've finally stepped foot into the Computer Science/Software Engineering
                 industry, and I was able to learn a ton! I'm glad, not only was I able to absorb a vast amount of technical
-                knowledge from my colleagues, I was also able to improve a lot of soft skills such as collaboration and communication.
+                knowledge from my colleagues, but I was also able to improve a lot of soft skills such as collaboration and communication.
                 Though, the best thing I've got to say about Tulip is the friends and connections I made; they taught me more than I could
                 have asked in the short time I was here, and I'm beyond grateful for them!
             </Content>
             <SubHeader>Acknowledgements</SubHeader>
             <Content>
                 I would like to express my deepest appreciation to my team lead, and friend, Keaton Neville. Not only did he teach me a wide range of
-                technical skills, he taught me how to better myself and I couldn't be more grateful. He was someone who understood me, and my mental
+                technical skills, but he also taught me how to better myself and I couldn't be more grateful. He was someone who understood me, and my mental
                 wellbeing, he was someone who I could genuinely relate to, and he was someone that I looked up to. I don't think I've ever met a more genuine, relatable,
-                and hard working person in my entire life, and I've got to say, that he really did change my life.
+                and hard-working person in my entire life, and I've got to say, that he did change my life.
             </Content>
             <Link to='/' style={{ textDecoration: 'none' }}>
                 <HomeButton variant='solid'>
